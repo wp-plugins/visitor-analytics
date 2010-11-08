4d198255235b2e1971e2f2e04bbbe520b2b0d850
violetcharm <violetcharm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-08 08:36:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/visitor-analytics/trunk@309122 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- va_option.php
+++ va_option.php
@@ -15,7 +15,7 @@ function va_option(){
 	echo '<form name="form1" method="post" action="'.$_SERVER['REQUEST_URI'].'">';
 	echo "<p>Numbers to show  perpage :<input type='text' size='10' name='va_per_page' value='".$va_options['per_page']."'/> </p>";	
 	echo '<p>Check IP Address :</p>';
-	echo "<input type='text' size='50' name='va_ip_addr' value='".$va_options['ip_addr']."'/> </p>";	
+	echo "<input type='text' size='100' name='va_ip_addr' value='".$va_options['ip_addr']."'/> </p>";	
 	echo '<p>Ip Ban List: </p>';
 	echo '<textarea style="width:400px;height:60px;" row="5" name="va_ban_ips" >'.$va_options['ban_ips'].'</textarea>';
 	echo '<p>Ban Message: </p>';
